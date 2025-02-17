1ff1fd20ce08c56af21745df0c4acdc8ebfcfa27
sebteouii <sebteouiikhoubayb@gmail.com>
2025-02-17 14:46:35 +0100

    props

---
+++ react_props/task_0/dashboard/src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="App-footer">
+      <p>Copyright 2024 - Holberton School</p>
+    </div>
+  );
+};
+
+export default Footer;
